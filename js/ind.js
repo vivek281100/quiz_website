0fb368a9b1b16bf74b07fa024ae5a413c6ae2fed
vivek281100 <88226750+vivek281100@users.noreply.github.com>
2021-12-01 18:36:44 +0530

    Add files via upload

---
+++ js/ind.js
@@ -0,0 +1,23 @@
+function valTrue() {
+    var radio = document.getElementById("true");
+    var text = document.getElementById("optTrue");
+    if (radio.checked == true){
+      text.style.display = "block";
+    } else {
+        text.style.display ="none";
+    }
+  }
+
+function valFalse() {
+    var radio = document.getElementById("false");
+    var text = document.getElementById("optFalse");
+    if (radio.checked == true){
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
+
+function ans(){
+    
+}
